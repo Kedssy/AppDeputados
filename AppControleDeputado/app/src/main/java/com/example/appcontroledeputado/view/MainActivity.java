e8f248397bba2cd4d63443fe833a1a2b9660fa9d
MichalskiLucas <lucasmichalski.pessoal@gmail.com>
2023-11-16 10:47:44 -0300

    Feat: Criado tela de listagem de deputados com seus nomes e partidos

--- AppControleDeputado/app/src/main/java/com/example/appcontroledeputado/view/MainActivity.java
+++ AppControleDeputado/app/src/main/java/com/example/appcontroledeputado/view/MainActivity.java
@@ -1,33 +1,46 @@
-package com.example.appcontroledeputado;
+package com.example.appcontroledeputado.view;
+
+import static com.example.appcontroledeputado.utils.Globais.listaDeputados;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.appcontroledeputado.R;
 import com.example.appcontroledeputado.controller.DeputadoController;
 import com.example.appcontroledeputado.dto.DadosDeputado;
 import com.example.appcontroledeputado.dto.DeputadoDto;
+import com.example.appcontroledeputado.utils.Globais;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btDespesas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btDespesas = findViewById(R.id.btDespesas);
+        btDespesas.setOnClickListener(v -> abrirActivity());
         DeputadoController.getDeputadoDto(this, new DeputadoController.DeputadoCallback() {
             @Override
             public void onDeputadoDtoReceived(DeputadoDto deputadoDto) {
                 // Utilize o deputadoDto retornado aqui
 
-                List<DadosDeputado> dadosDeputadoList = new ArrayList<>();
+                //List<DadosDeputado> dadosDeputadoList = new ArrayList<>();
 
-                dadosDeputadoList.addAll(deputadoDto.getDados());
+                //dadosDeputadoList.addAll(deputadoDto.getDados());
 
-                System.out.println("DeputadoDto. Email do deputado: " + dadosDeputadoList.get(1).getEmail());
+                listaDeputados = new ArrayList<>();
+
+                listaDeputados.addAll(deputadoDto.getDados());
+
+                System.out.println("DeputadoDto. Email do deputado: " + listaDeputados.get(1).getEmail());
             }
 
             @Override
@@ -37,4 +50,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void abrirActivity(){
+        Intent intent = new Intent(this, ListaDeputadoActivity.class);
+        startActivity(intent);
+    }
+
 }
